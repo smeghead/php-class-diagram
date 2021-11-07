73d51b7dad66fb0ecc78dfc5b0dd91b16bd1039d
smeghead <smeghead7@gmail.com>
2021-11-07 22:04:30 +0900

    delete lines never run.

--- src/DiagramElement/ArrowInheritance.php
+++ src/DiagramElement/ArrowInheritance.php
@@ -7,9 +7,6 @@ class ArrowInheritance extends Arrow {
     protected string $figure = '<|--';
 
     public function toString(): string {
-        if (strpos($this->to, '[]') === false) {
-            return sprintf('  %s %s %s', $this->to, $this->figure, $this->from);
-        }
-        return sprintf('  %s "1" %s "*" %s', $this->to, $this->figure, str_replace('[]', '', $this->from));
+        return sprintf('  %s %s %s', $this->to, $this->figure, $this->from);
     }
 }
