7c3a613318e963d0a5032fdd52e73ee48ed20443
smeghead <smeghead7@gmail.com>
2023-02-02 01:00:51 +0900

    namespace parameters

--- test/PhpTypeExpressionTest.php
+++ test/PhpTypeExpressionTest.php
@@ -20,7 +20,7 @@ final class PhpTypeExpressionTest extends TestCase {
             throw new \Exception("Parse error: {$error->getMessage()} file: {$filename}\n");
         }
 
-        $expression = new PhpTypeExpression($ast[0]->stmts[1]->stmts[0], PhpTypeExpression::TYPE);
+        $expression = new PhpTypeExpression($ast[0]->stmts[1]->stmts[0], PhpTypeExpression::TYPE, ['hoge', 'fuga', 'product']);
         $types = $expression->getTypes();
 
         $this->assertSame([], $types[0]->getNamespace(), 'namespace');
@@ -35,8 +35,7 @@ final class PhpTypeExpressionTest extends TestCase {
         } catch (Error $error) {
             throw new \Exception("Parse error: {$error->getMessage()} file: {$filename}\n");
         }
-// var_dump($ast[0]->stmts[1]->stmts[1]);die();
-        $expression = new PhpTypeExpression($ast[0]->stmts[1]->stmts[1], PhpTypeExpression::TYPE);
+        $expression = new PhpTypeExpression($ast[0]->stmts[1]->stmts[1], PhpTypeExpression::TYPE, ['hoge', 'fuga', 'product']);
         $types = $expression->getTypes();
 
         $this->assertSame([], $types[0]->getNamespace(), 'namespace');
@@ -45,5 +44,50 @@ final class PhpTypeExpressionTest extends TestCase {
         $this->assertSame([], $types[1]->getNamespace(), 'namespace');
         $this->assertSame('string', $types[1]->getName(), 'name');
         $this->assertSame(false, $types[1]->getNullable(), 'nullable');
+    }
+    public function testPrice(): void {
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        $filename = sprintf('%s/php8/product/Product.php', $this->fixtureDir);
+        try {
+            $ast = $parser->parse(file_get_contents($filename));
+        } catch (Error $error) {
+            throw new \Exception("Parse error: {$error->getMessage()} file: {$filename}\n");
+        }
+        $expression = new PhpTypeExpression($ast[0]->stmts[1]->stmts[2], PhpTypeExpression::TYPE, ['hoge', 'fuga', 'product']);
+        $types = $expression->getTypes();
+
+        $this->assertSame(['hoge', 'fuga', 'product'], $types[0]->getNamespace(), 'namespace');
+        $this->assertSame('Name', $types[0]->getName(), 'name');
+        $this->assertSame(false, $types[0]->getNullable(), 'nullable');
+    }
+    public function testException(): void {
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        $filename = sprintf('%s/php8/product/Product.php', $this->fixtureDir);
+        try {
+            $ast = $parser->parse(file_get_contents($filename));
+        } catch (Error $error) {
+            throw new \Exception("Parse error: {$error->getMessage()} file: {$filename}\n");
+        }
+        $expression = new PhpTypeExpression($ast[0]->stmts[1]->stmts[4], PhpTypeExpression::TYPE, ['hoge', 'fuga', 'product']);
+        $types = $expression->getTypes();
+
+        $this->assertSame([], $types[0]->getNamespace(), 'namespace');
+        $this->assertSame('Exception', $types[0]->getName(), 'name');
+        $this->assertSame(false, $types[0]->getNullable(), 'nullable');
+    }
+    public function testRelated(): void {
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        $filename = sprintf('%s/php8/product/Product.php', $this->fixtureDir);
+        try {
+            $ast = $parser->parse(file_get_contents($filename));
+        } catch (Error $error) {
+            throw new \Exception("Parse error: {$error->getMessage()} file: {$filename}\n");
+        }
+        $expression = new PhpTypeExpression($ast[0]->stmts[1]->stmts[5], PhpTypeExpression::TYPE, ['hoge', 'fuga', 'product']);
+        $types = $expression->getTypes();
+
+        $this->assertSame(['hoge', 'fuga', 'product', 'bar'], $types[0]->getNamespace(), 'namespace');
+        $this->assertSame('Boo', $types[0]->getName(), 'name');
+        $this->assertSame(false, $types[0]->getNullable(), 'nullable');
     }
 }
