fae9d6da0a8efac29a162b8ce215edf996e29c36
smeghead <smeghead7@gmail.com>
2023-04-23 22:11:39 +0900

    update create image script.

--- update-dogfood-images.sh
+++ update-dogfood-images.sh
@@ -12,7 +12,7 @@ $SCRIPT_DIR/bin/php-class-diagram \
 	--disable-class-methods $SCRIPT_DIR/src/DiagramElement \
 	| plantuml -charset utf-8 -pipe -tpng > $SCRIPT_DIR/dogfood-model.png
 $SCRIPT_DIR/bin/php-class-diagram \
-	--package-diagram $SCRIPT_DIR/src/DiagramElement \
+	--package-diagram $SCRIPT_DIR/src \
 	| plantuml -charset utf-8 -pipe -tpng > $SCRIPT_DIR/dogfood-package.png
 $SCRIPT_DIR/bin/php-class-diagram \
 	--division-diagram $SCRIPT_DIR/test/fixtures/enum \
