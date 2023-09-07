482a4fdb1802812a9ea005ddcea78c6fc23af3c8
smeghead <smeghead7@gmail.com>
2023-09-07 21:52:16 +0900

    v1.2.1
    Bug fix
    
     * fix to Avoid interdependencies Smeghead\PhpClassDiagram\Php and Smeghead\PhpClassDiagram\Php\Finders.

--- src/Main.php
+++ src/Main.php
@@ -14,7 +14,7 @@ use Smeghead\PhpClassDiagram\Php\PhpReader;
 
 class Main
 {
-    const VERSION = 'v1.2.0';
+    const VERSION = 'v1.2.1';
 
     public function __construct(string $directory, Options $options)
     {
